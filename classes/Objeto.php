dd2708dd4e2f19da7780c301d062f4cecd573bba
dberruezo <davidberruezo@davidberruezo.com>
2020-04-08 19:58:13 +0200

    update

---
+++ classes/Objeto.php
@@ -0,0 +1,10 @@
+<?php 
+namespace classes;
+class Objeto
+{
+    private $my_variable;
+    public function __construct(){
+        echo "construct object<br>";
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include_once("vendor/autoload.php"); ?>
+<?php use classes\Objeto; ?>
+<?php $objeto = new Objeto(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+    <meta name="description" content="David Berruezo application">
+    <meta name="author" content="David Berruezo">
+    <meta name="designer" content="David Berruezo">
+    <meta name="reply-to" content="davidberruezog@davidberruezo">
+    <meta name="copyright" content="(c) 2020 David Berruezo - www.davidberruezo.com">
+    <meta name="robots" content="index, follow">
+    <!--Verifitication-->
+    <meta name="google-site-verification" content="">
+    <meta name="alexaVerifyID" content="">
+    <meta name="yandex-verification" content="">
+    <meta name="msvalidate.01" content="">
+    <!-- / Verifitication-->
+    <link rel="shortcut icon" href="./images/icons/favicon.ico">
+    <link rel="canonical" href="http://www.domainname.com/">
+    <!-- CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700" rel="stylesheet" type="text/css">
+    <link href="./node_modules/normalize.css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="./node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+</head>
+<body>
+<h1>HTML5 & PHP index</h1>    
+</body>
+</html>
